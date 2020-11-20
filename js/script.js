8d8a703b7f4f3d8c19c67cfdc237fbe96da1b083
Caroline Forget <caro.forget23@gmail.com>
2020-11-20 08:51:56 -0500

    Creation d'un objet avec une classe.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
+    //-------------------------------------- Le jeu
+    class Jeu{ // Declaration
+        constructor() {
+            console.log("Jeu");
+        }
+    }
+
+    //-------------------------------------- Le serpent
+    class Serpent{
+        constructor() {
+            console.log("Serpent");
+        }
+    }
+
+    //-------------------------------------- La pomme
+    class Pomme{
+        constructor() {
+            console.log("Pomme");
+        }
+    }
+
+    var unePartie = new Jeu(); // Creation d'un objet avec une classe.
 
 });
