137428055c221f6ebf6e189a103fffefd43e5747
Caroline Forget <caro.forget23@gmail.com>
2020-11-22 11:21:39 -0500

    Creer une nouvellePartie();

--- js/script.js
+++ js/script.js
@@ -10,18 +10,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
             this.sortiePointage = document.querySelector(_idPointage);
             this.grandeurCarre = 20;
             this.grandeurGrille = 15;
+
         }
 
-        nouvellePartie(){
+        nouvellePartie(){ //------- Cree les objets sur la scene.
 
+            this.affichagePointage(1);
+            this.pomme = new Pomme();
+            this.serpent = new Serpent();
         }
 
         finPartie(){
 
         }
 
-        affichagePointage(){
-            
+        affichagePointage(_lePointage){
+            this.sortiePointage.innerHTML = _lePointage;
         }
     }
 
@@ -39,6 +43,19 @@ document.addEventListener("DOMContentLoaded", function(event) {
         }
     }
 
-    var unePartie = new Jeu("#jeu", "#pointage"); // Creation d'un objet avec une classe. --- #jeu == id pour le jeu -- APPELLE LE CONSTRUCTOR
+    var unePartie = new Jeu("#jeu", "#pointage"); // Creation d'un objet avec une classe. (le jeu) --- #jeu == id pour le jeu -- APPELLE LE CONSTRUCTOR DE LA CLASSE JEU
+
+
+    //-------------------------------------- Faire la nouvellePartie
+    var btnJouer = document.querySelector("#btnJouer"); // Recupere le btnJouer.
+    btnJouer.addEventListener("click", nouvellePartie);
+
+    function nouvellePartie(){
+        unePartie.nouvellePartie(); // Car unePartie == objet Jeu();
+    }
+
+
+
+
 
 });
